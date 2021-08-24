15776c1c7a03ef13a8d52b2a655411d61c698883
Muhammet <mohammad.nadom98@gmail.com>
2021-08-24 16:48:42 +0300

    a

--- db.js
+++ db.js
@@ -1,31 +1,31 @@
 // const Pool = require("pg").Pool;
 const { Pool } = require('pg')
 
-const { Client } = require("pg");
-
-const pool = new Pool({
-  host: "localhost",
-  user: 'postgres',
-  password: "crossmyheart1243",
-  port: 5432,
-  database: "varoliletisim",
-  max: 20,
-  connectionTimeoutMillis: 2000
-})
+// const { Client } = require("pg");
 
 // const client = new Client({
-//     user: "postgres",
+  //     user: "postgres",
 //     password: "crossmyheart1243",
 //     host: "localhost",
 //     port: 5432,
 //     database: "varoliletisim"
 // })
 
-const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
-    }
-  });
+/////////////////////////////////
+// const pool = new Pool({
+//   host: "localhost",
+//   user: 'postgres',
+//   password: "crossmyheart1243",
+//   port: 5432,
+//   database: "varoliletisim",
+//   max: 20,
+//   connectionTimeoutMillis: 2000
+// })
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
-module.exports = client;
+module.exports = pool;
