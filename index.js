d10155fed93d1a22a96a7b76c8d4dbb7db448cae
MilesTheEdgy <mohammad.nadom98@gmail.com>
2021-09-19 13:12:39 +0300

    pushing changes

--- index.js
+++ index.js
@@ -1,44 +1,66 @@
 //are you okay brah
-const Bree = require("bree")
+const Bree = require("bree");
 const express = require("express");
 const app = express();
-const pool = require("./db");
-const path = require("path")
+const pool = require("./controller/database");
+const path = require("path");
 
-const verifyRoute = require("./controller/routes/verify")
-const generalRoute = require("./controller/routes/app")
-const dealerRoute = require("./controller/routes/dealer")
-const sdRoute = require("./controller/routes/sd")
-const sdcRoute = require("./controller/routes/sdc")
+const verifyRoute = require("./controller/routes/verify");
+const generalRoute = require("./controller/routes/app");
+const dealerRoute = require("./controller/routes/dealer");
+const sdRoute = require("./controller/routes/sd");
+const sdcRoute = require("./controller/routes/sdc");
 
-const bree = new Bree({
+function startServer() {
+  const bree = new Bree({
     jobs: [
       {
-        name: 'printreport',
-        cron: "0 2 1 * *"
-      }
-    ]
+        name: "printreport",
+        cron: "0 2 1 * *",
+      },
+    ],
   });
-bree.start();
+  bree.start();
 
-app.use(express.json());
-app.use(express.static(path.join(__dirname, "client", "build")));
-app.use(express.static("public"));
-app.use(verifyRoute)
-app.use(generalRoute)
-app.use(dealerRoute)
-app.use(sdRoute)
-app.use(sdcRoute)
+  app.use(express.json());
+  app.use(express.static(path.join(__dirname, "client", "build")));
+  app.use(express.static("public"));
+  app.use(verifyRoute);
+  app.use(generalRoute);
+  app.use(dealerRoute);
+  app.use(sdRoute);
+  app.use(sdcRoute);
+  
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+  const PORT = process.env.PORT || 8080;
+  app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
+}
 
-pool.on('error', (err, client) => {
-    console.error('Unexpected error on idle client', err)
-    process.exit(-1)
-})
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-})
+async function connectDB() {
+  let retries = 5;
+  while (retries) {
+    try {
+      const client = await pool.connect();
+      await client.query("SELECT NOW()");
+      console.log("connected to database");
+      client.release();
+      // Start express server
+      startServer();
+      break;
+    } catch (error) {
+      console.log("Postgres database error: ", error);
+      retries -= 1;
+      console.log("retries left: " + retries);
+      await new Promise((res) => setTimeout(res, 5000));
+    }
+  }
+}
 
-const PORT = process.env.PORT || 8080
-console.log("///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////")
-app.listen(PORT, ()=>console.log(`Server is listening on port ${PORT}`));
+(async function () {
+  // MAIN
+  // Establish a database connection
+  await connectDB();
+})();
