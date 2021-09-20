0b6fd896d5cf696fa174a22905eb0b126b909416
MilesTheEdgy <mohammad.nadom98@gmail.com>
2021-09-20 21:21:34 +0300

    a

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,9 +2,9 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/b2b-iys',
     createProxyMiddleware({
-      target: 'https://projects.muhammed-aldulaimi.com',
+      target: 'https://projects.muhammed-aldulaimi.com/',
+      // pathRewrite: {'^/': '/b2b-iys/'},
       changeOrigin: true,
     })
   );
